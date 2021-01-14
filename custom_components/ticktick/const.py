f25b781ea33b9d324e20d830877e92e6a88d5c03
Bauke Posthuma <baukeposthuma@gmail.com>
2021-01-14 16:12:22 +0100

    update homeassistant

---
+++ custom_components/ticktick/const.py
@@ -0,0 +1,8 @@
+"""Constants for the TickTick integration."""
+
+DOMAIN = "ticktick"
+
+TICKTICK_HOST = "ticktick.com"
+AUTH_URL = "/api/v2/user/signon?wc=true&remember=true"
+ADD_TASK_URL = "/api/v2/task"
+PROJECT_URL = "/api/v2/batch/check/0"
